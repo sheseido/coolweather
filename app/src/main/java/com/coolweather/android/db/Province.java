8380a222ef3b8a8f19244451f571acfab2ffa2ba
chenhu <huchen@happyju.com>
2018-08-15 18:22:20 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -0,0 +1,33 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String privinceName;
+    private int privinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrivinceName() {
+        return privinceName;
+    }
+
+    public void setPrivinceName(String privinceName) {
+        this.privinceName = privinceName;
+    }
+
+    public int getPrivinceCode() {
+        return privinceCode;
+    }
+
+    public void setPrivinceCode(int privinceCode) {
+        this.privinceCode = privinceCode;
+    }
+}
